64d08e24b2811366944c1d266a845a7ddef2026c
Рамис <doof3241@gmail.com>
2026-02-04 03:16:59 +0300

    Added all enemy types

---
+++ TowerDefenceProject/Assets/_Project/Scripts/EnemiesManagement/EnemyTypes/Cactus.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Scripts.EnemiesManagement.EnemyTypes
+{
+    public class Cactus : Enemy
+    {
+
+    }
+}
---
+++ TowerDefenceProject/Assets/_Project/Scripts/EnemiesManagement/EnemyTypes/Mushroom.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Scripts.EnemiesManagement.EnemyTypes
+{
+    public class Mushroom : Enemy
+    {
+
+    }
+}
