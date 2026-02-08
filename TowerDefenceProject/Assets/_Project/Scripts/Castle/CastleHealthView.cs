634def22e23f98847a3fb9edc1f716407279708c
Рамис <doof3241@gmail.com>
2026-02-09 01:45:24 +0300

    Update health and moneys view

--- TowerDefenceProject/Assets/_Project/Scripts/Castle/CastleHealthView.cs
+++ TowerDefenceProject/Assets/_Project/Scripts/Castle/CastleHealthView.cs
@@ -3,29 +3,35 @@ using Reflex.Attributes;
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Project.Scripts.Castle
 {
     public class CastleHealthView : MonoBehaviour, IDisposable
     {
         [SerializeField] private TextMeshProUGUI _currentHealthText;
+        [SerializeField] private Button _addHealthByAdButton;
 
         private IDisposable _disposable;
         private CastleHealthManagement _castleHealthManagement;
 
+        private readonly Subject<Unit> OnAddHealthByAdButtonClicked = new();
+
+        public ISubject<Unit> ReadOnlyOnAddHealthByAdButtonClicked => OnAddHealthByAdButtonClicked;
+
         public void Dispose()
         {
             _disposable?.Dispose();
+            OnAddHealthByAdButtonClicked?.Dispose();
         }
 
         [Inject]
         private void Initialize(CastleHealthManagement castleHealthManagement)
         {
             _castleHealthManagement = castleHealthManagement;
-
-
-
             _disposable = _castleHealthManagement.ReadOnlyCurrentHealth.Subscribe(OnCastleHealthChanged);
+
+            _addHealthByAdButton.onClick.AddListener(() => OnAddHealthByAdButtonClicked?.OnNext(Unit.Default));
         }
 
         private void OnCastleHealthChanged(int currentHealth)
--- TowerDefenceProject/Assets/_Project/Scripts/MoneySystem/MoneyView.cs
+++ TowerDefenceProject/Assets/_Project/Scripts/MoneySystem/MoneyView.cs
@@ -3,6 +3,7 @@ using Reflex.Attributes;
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace _Project.Scripts.MoneySystem
 {
@@ -11,8 +12,13 @@ namespace _Project.Scripts.MoneySystem
         private readonly CompositeDisposable Disposables = new();
 
         [SerializeField] private TextMeshProUGUI _amountOfMoneyText;
+        [SerializeField] private Button _onAddMoneyByAdButton;
 
         private MoneyManagement _moneyManagement;
+
+        private readonly Subject<Unit> OnAddMoneyByAdButtonClicked = new();
+
+        public ISubject<Unit> ReadOnlyOnAddMoneyByAdButtonClicked => OnAddMoneyByAdButtonClicked;
 
         private void Awake()
         {
@@ -22,6 +28,7 @@ namespace _Project.Scripts.MoneySystem
         public void Dispose()
         {
             Disposables.Dispose();
+            OnAddMoneyByAdButtonClicked?.Dispose();
         }
 
         private void OnCurrentAmounfOfMoneyChanged(int amountOfMoney)
@@ -37,6 +44,8 @@ namespace _Project.Scripts.MoneySystem
             _moneyManagement.ReadOnlyCurrentAmountOfMoney
                 .Subscribe(OnCurrentAmounfOfMoneyChanged)
                 .AddTo(Disposables);
+
+            _onAddMoneyByAdButton.onClick.AddListener(() => OnAddMoneyByAdButtonClicked?.OnNext(Unit.Default));
         }
     }
 }
